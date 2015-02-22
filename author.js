1aadde1cb5eee1f6ee8d378da258a57c0ca6ba08
Gautama <gswamina@gmail.com>
2015-02-21 22:02:03 -0800

    start parsing book

--- author.js
+++ author.js
@@ -1,6 +1,30 @@
+var _ = require('underscore');
 var fs = require('fs');
+
+var logger = {
+	info: function(message) {
+		console.log(message);
+	},
+	debug: function(message) {
+		// console.log(message);
+	},
+	error: function(message) {
+		console.log(message);
+	}
+}
+
+var book = {};
+function parseBook(bookManifest) {
+	logger.debug('================================');
+	logger.debug(JSON.stringify(bookManifest));
+	logger.debug('================================');
+
+	book.title = bookManifest.title;
+	logger.info(book.title);
+}
 
 var manifestFileName = process.argv[2];
 console.log('reading manifestFile=' + manifestFileName, 'utf-8');
-var book = JSON.parse(fs.readFileSync(manifestFileName));
+var bookManifest = JSON.parse(fs.readFileSync(manifestFileName));
 
+parseBook(bookManifest);
