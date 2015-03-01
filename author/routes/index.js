6edc6547856a6f2d76a9470a15d02e3b44ac52a4
Gautama <gswamina@gmail.com>
2015-02-28 18:21:11 -0800

    join express and author at the hip

--- author/routes/index.js
+++ author/routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+
+var logger = {
+    info: function(message) {
+        console.log(message);
+    },
+    debug: function(message) {
+       // console.log(message);
+    },
+    verbose: function(message) {
+        console.log(message);
+    },
+    error: function(message) {
+        console.log(message);
+    }
+}
+
+var u = {
+    prettyJson: function (jObj) {
+        return JSON.stringify(jObj, null, '\t');
+    }
+}
+
+var AUTHOR = {
+    book: undefined,
+    config: {
+        manifest: 'japan_story.json'
+    }
+}
+
+function parseBook(bookManifest) {
+    console.log('reading bookManifest=' + bookManifest, 'utf-8');
+    var bookModel = JSON.parse(fs.readFileSync(bookManifest));
+
+    logger.debug('========================');
+    logger.debug(u.prettyJson(bookModel));
+    logger.debug('========================');
+
+    return bookModel;
+}
+AUTHOR.book = parseBook(AUTHOR.config.manifest);
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: AUTHOR.book.title });
 });
 
 module.exports = router;
